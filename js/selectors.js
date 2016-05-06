53ead0cc958e43c75e86cec7cce225ae9d669db4
lindsaysteinbach <lsteinbach@google.com>
2016-05-06 15:25:35 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -6,3 +6,14 @@ function pSelector(){
 }
 */
 
+function h1Selector() {
+  return $('h1');
+}
+
+function liInOlSelector() {
+  return $('ol li');
+}
+
+function linkSelector {
+  return $("#my-id.my-class a");
+}
